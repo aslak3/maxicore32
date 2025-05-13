7f000f8f215e4b745be8a88fd592cdce098f914c
Lawrence Manning <lawrence@aslak.net>
2025-05-13 22:45:32 +0100

    Started a basic parser
    Can produce a stream of hex, along with the type (byte, word or long).

--- asm/uart_test.asm
+++ asm/uart_test.asm
@@ -15,69 +15,135 @@ I2C_STATUS_OF=I2C_CONTROL_OF
 UART_STATUS_OF=0x2c
 UART_DATA_OF=0x30
 
-ASCII_a=0x61
+ASCII_SP=0x20
+ASCII_EXP=0x21
 ASCII_0=0x30
+ASCII_COLON=0x3a
+ASCII_A=0x41
+ASCII_a=0x61
 
 ; fixed register usage: all other register usage is fairly adhoc
 ;   r15=stack, r14=return address, r13=vars pointer, r11=io base address
-;   r8, r9 and r10 are scratch and neither saved or restored
+; r4, r5 are scracth and are never saved.
 
 start:          loadi.u r15,stack                                   ; setup stack pointer
                 loadi.u r14,0                                       ; return address
                 loadi.l r11,IO_BASE
 
-; .loop:          loadi.u r0,prompt
-;                 callbranch r14,putstring
-;                 loadi.u r0,buffer
-;                 callbranch r14,getstring
-;                 loadi.u r0,youtyped
-;                 callbranch r14,putstring
-;                 loadi.u r0,buffer
-;                 callbranch r14,putstring
-;
-;                 branch .loop
+.loop:          loadi.u r8,prompt
+                callbranch r14,putstring
 
-                loadi.l r8,0xfffff
-.loop:          loadi.u r1,buffer
-                copy r0,r8
-                callbranch r14,longtoascii
+                loadi.u r8,inputbuffer
+                callbranch r14,getstring
+
+                loadi.u r8,outputbuffer
+                loadi.u r9,youtyped
+                callbranch r14,concatstr
+                loadi.u r9,inputbuffer
+                callbranch r14,concatstr
+                loadi.u r9,crlf
+                callbranch r14,concatstr
+                loadi.u r8,outputbuffer
+                callbranch r14,putstring
+
+                loadi.u r8,inputbuffer
+                loadi.u r10,parsedbuffer
+.parse_loop:    load.bu r0,(r8)
+                nop
+                test r0,r0
+                nop
+                branch.eq .parse_done
+                compare r0,r0,ASCII_SP
+                nop
+                branch.eq .sp_hop
+.sp_hop_cont:   callbranch r14,asciitoint
+                test r1,r1
+                nop
+                branch.eq .parse_done
+                store.l 0(r10),r1                           ; type
+                store.l 4(r10),r0                           ; value
+                add r10,r10,8
+                branch .parse_loop
+.sp_hop:        add r8,r8,1
+                branch .sp_hop_cont
+
+.parse_done:    loadi.u r1,0
                 loadi.u r0,0
+                store.l 0(r10),r1                           ; type
+                store.l 4(r10),r0                           ; value
+
+                loadi.u r10,parsedbuffer
                 nop
-                store.b (r1),r0
-                loadi.u r0,buffer
-                callbranch r14,putstring
-                loadi.u r0,crlf
-                callbranch r14,putstring
-                test r8,r8
+.print_loop:    load.l r0,0(r10)
                 nop
-                branch.eq .done
-                sub r8,r8,0x1
-                branch .loop
+                test r0,r0
+                nop
+                branch.eq .print_out
+
+                loadi.u r8,outputbuffer
+                loadi.u r9,type
+                callbranch r14,concatstr
+                load.l r0,0(r10)
+                callbranch r14,bytetoascii
+                loadi.u r9,crlf
+                callbranch r14,concatstr
+                loadi.u r8,outputbuffer
+                callbranch r14,putstring
+
+                loadi.u r8,outputbuffer
+                loadi.u r9,value
+                callbranch r14,concatstr
+                load.l r0,4(r10)
+                callbranch r14,longtoascii
+                loadi.u r9,crlf
+                callbranch r14,concatstr
+                loadi.u r8,outputbuffer
+                callbranch r14,putstring
+
+                add r10,r10,8
+
+                branch .print_loop
+
+.print_out:     branch .loop
 
 .done:          halt
 
-; outputs the null terminated at r0 to the terminal
-putstring:      sub r15,r15,4
+; general guidance:
+; r8 = string pointer
+
+; concatenate the string in r9 onto the string at r8, returning a pointer in r8 to the null that was added
+concatstr:      load.bu r4,(r9)                                     ; get the byte to add from the source
+                add r8,r8,1                                         ; advance dst
+                add r9,r9,1                                         ; advance src
+                test r4,r4                                          ; test the byte we are about to add
+                store.b -1(r8),r4                                   ; add the byte and the pos pre increment
+                branch.ne concatstr                                 ; add more if byte wasn't null
+                sub r8,r8,1                                         ; point at the null
+                jump r14
+
+; outputs the null terminated at r8 to the terminal
+putstring:      sub r15,r15,8
                 nop
+                store.l 4(r15),r0
                 store.l 0(r15),r14                                  ; save current return address
-                copy r2,r0                                          ; r2 is now the string pointer
                 nop
-.loop:          load.bu r0,(r2)
-                add r2,r2,1
+.loop:          load.bu r0,(r8)
+                add r8,r8,1
                 test r0,r0
                 nop
                 branch.eq .out
                 callbranch r14,putchar
                 branch .loop
 .out:           load.l r14,0(r15)
-                add r15,r15,4
+                load.l r0,4(r15)
+                add r15,r15,8
                 jump r14
 
-; inputs a cr or lf temrminated line into r0, which is moved to the end
-getstring:      sub r15,r15,4
+; inputs a cr or lf temrminated line into r8, which is moved to the end
+getstring:      sub r15,r15,8
                 nop
+                store.l 4(r15),r0
                 store.l 0(r15),r14                                  ; save current return address
-                copy r2,r0
 .loop:          callbranch r14,getchar
                 compare r0,r0,0x0d
                 nop
@@ -86,96 +152,176 @@ getstring:      sub r15,r15,4
                 nop
                 branch.eq .eol
                 callbranch r14,putchar
-                store.b (r2),r0
-                add r2,r2,1
+                store.b (r8),r0
+                add r8,r8,1
                 branch .loop
 .eol:           loadi.u r0,0                                        ; addding a null
                 nop
-                store.b (r2),r0
+                store.b (r8),r0
                 load.l r14,0(r15)
-                add r15,r15,4
+                load.l r0,4(r15)
+                add r15,r15,8
                 jump r14
 
 ; outputs the byte in r0
-putchar:        load.bu r1,UART_STATUS_OF(r11)                      ; get the current status
+putchar:        load.bu r4,UART_STATUS_OF(r11)                      ; get the current status
                 nop
-                bit r1,r1,0b00100000
+                bit r4,r4,0b00100000
                 nop
                 branch.eq putchar                                   ; loop back if not ready
                 store.b UART_DATA_OF(r11),r0
                 jump r14
 
 ; inputs the byte in r0
-getchar:        load.bu r1,UART_STATUS_OF(r11)                      ; get the current status
+getchar:        load.bu r4,UART_STATUS_OF(r11)                      ; get the current status
                 nop
-                bit r1,r1,0b10000000
+                bit r4,r4,0b10000000
                 nop
                 branch.eq getchar                                   ; loop back if not read
                 load.bu r0,UART_DATA_OF(r11)
                 jump r14
 
-nybbletoascii:  compare r0,r0,10                                    ; <10?
+; converts the nybble in r0 to hex, writing it into r8 and advancing it 1 byte
+_nybbletoascii: compare r0,r0,10                                    ; <10?
                 nop
                 branch.lt .lt_10                                    ; yes, only add 0
                 add r0,r0,ASCII_a-ASCII_0-10                        ; add past 'a', but less '0'
                 nop
 .lt_10:         add r0,r0,ASCII_0                                   ; add '0' too
                 nop
-                store.b (r1),r0                                     ; save that nybble
-                add r1,r1,1
+                store.b (r8),r0                                     ; save that nybble
+                add r8,r8,1
                 jump r14
 
-; convert the byte in r0 to hex, writing it into r1 and advancing it 2 bytes
+; convert the byte in r0 to hex, writing it into r8 and advancing it 2 bytes
 bytetoascii:    sub r15,r15,8
                 nop
-                store.l 0(r15),r8
-                store.l 4(r15),r14                                  ; save current return address
-                unsignextb r8,r0                                          ; save original
+                store.l 0(r15),r14
+                store.l 4(r15),r1
+                unsignextb r1,r0                                    ; save original
                 logicright r0,r0,4                                  ; get the left most nybble
-                callbranch r14,nybbletoascii
-                and r0,r8,0x0f                                      ; mask off the left nybble
-                callbranch r14,nybbletoascii
-                copy r0,r8                                    ; restore r0
-                load.l r14,4(r15)
-                load.l r8,0(r15)
+                callbranch r14,_nybbletoascii
+                and r0,r1,0x0f                                      ; mask off the left nybble
+                callbranch r14,_nybbletoascii
+                copy r0,r1                                          ; restore r0
+                loadi.u r1,0
+                nop
+                store.b (r8),r1                                     ; null terminate
+                load.l r1,4(r15)
+                load.l r14,0(r15)
                 add r15,r15,8
                 jump r14
 
-; convert the word in r0 to hex, writing it into r1 and advancing it 4 bytes
+; convert the word in r0 to hex, writing it into r8 and advancing it 4 bytes
 wordtoascii:    sub r15,r15,8
                 nop
-                store.l 4(r15),r8
-                store.l 0(r15),r14                                  ; save current return address
-                unsignextw r8,r0
+                store.l 0(r15),r14
+                store.l 4(r15),r1
+                unsignextw r1,r0
                 logicright r0,r0,8                                  ; get the top byte
                 callbranch r14,bytetoascii
-                unsignextb r0,r8
+                unsignextb r0,r1
                 callbranch r14,bytetoascii
-                copy r0,r8                                          ; restore r0
+                copy r0,r1                                          ; restore r0
+                load.l r1,4(r15)
                 load.l r14,0(r15)
-                load.l r8,4(r15)
                 add r15,r15,8
                 jump r14
 
+; convert the long in r0 to hex, writing it into r8 and advancing it 8 bytes
 longtoascii:    sub r15,r15,8
                 nop
-                store.l 4(r15),r8
-                store.l 0(r15),r14                                  ; save current return address
-                copy r8,r0
+                store.l 0(r15),r14
+                store.l 4(r15),r1                                  ; save current return address
+                copy r1,r0
                 logicright r0,r0,16
                 callbranch r14,wordtoascii
-                unsignextw r0,r8
+                unsignextw r0,r1
                 callbranch r14,wordtoascii
-                copy r0,r8                                          ; restore r0
+                copy r0,r1                                          ; restore r0
+                load.l r1,4(r15)
                 load.l r14,0(r15)
-                load.l r8,4(r15)
                 add r15,r15,8
+                jump r14
+
+datatypetable:  #d8 0                                               ; 0
+
+                #d8 1                                               ; 1
+                #d8 1                                               ; 2
+
+                #d8 2                                               ; 3
+                #d8 2                                               ; 4
+
+                #d8 3                                               ; 5
+                #d8 3                                               ; 6
+                #d8 3                                               ; 7
+                #d8 3                                               ; 8
+
+                #d8 0                                               ; padding
+                #d8 0
+                #d8 0
+
+; convert the string at r8 to a integer. r0 will hold the value, r1 will
+; hold the type (1=byte, 2=word, 3=long). on error, r1 will be 0.
+; r2 will be moved to the first non printable char. sets zero on error.
+
+asciitoint:     sub r15,r15,4
+                nop
+                store.l 0(r15),r3                                   ; save r3 as we mess with it
+                loadi.u r0,0                                        ; set result to zero
+                loadi.u r1,0                                        ; clear digit counter
+                branch .next_char                                   ; branch into loop
+.top_of_loop:   sub r3,r3,ASCII_0                                   ; subtract '0' - r3 is this char
+                nop
+                branch.lt .bad                                      ; <0? that's bad_4
+                compare r3,r3,0x09                                  ; less than or equal to 0?
+                nop
+                branch.ls .next_nyb                                 ; yes? we are done with this
+                sub r3,r3,ASCII_A-ASCII_COLON                       ; subtract diff A - :
+                nop
+                branch.lt .bad                                      ; <0? bad
+                compare r3,r3,0x10                                 ; see if it is uppercase
+                nop
+                branch.lt .next_nyb                                 ; was uppercase
+                sub r3,r3,ASCII_a-ASCII_A                           ; was lowercase
+                nop
+                compare r3,r3,0x10                                  ; compare with upper range
+                nop
+                branch.ge .bad                                      ; >15? bad
+.next_nyb:      logicleft r0,r0,4                                   ; shift val to next nybble
+                nop
+                add r0,r0,r3                                        ; accumulate number
+                add r1,r1,1                                         ; inc digit number
+                nop
+                compare r1,r1,0x08                                  ; too many digits?
+                nop
+                branch.gt .bad                                      ; yes? bad
+.next_char:     load.bu r3,(r8)                                     ; get the next character
+                add r8,r8,1                                         ; move to next position
+                compare r3,r3,ASCII_EXP                             ; see if its a nonwsp char
+                nop
+                branch.ls .out                                      ; yes? then we are done
+                branch .top_of_loop                                 ; back for more digits
+.bad:           loadi.u r1,0                                        ; mark 0 digits
+.out:           loadi.u r3,datatypetable                            ; get start of table
+                nop
+                load.bu r1,r1(r3)                                   ; translate to type
+                sub r8,r8,1                                         ; wind back to space char or null
+                load.l r3,0(r15)                                    ; restore r3
+                add r15,r15,4
                 jump r14
 
                 #res 32
 stack:
 
-prompt:         #d "\r\n\r\nHello! Type a message: \0"
-buffer:         #res 80
+inputbuffer:    #res 64
+outputbuffer:   #res 64
+parsedbuffer:   #res 128
+
+prompt:         #d "\r\n\r\nHello! Type a number: \0"
 youtyped:       #d "\r\nYou typed: \0"
+type:           #d "Type: \0"
+value:          #d "Value: \0"
 crlf:           #d "\r\n\0"
+
+test_numbers:   #d "CABBA6e\0"
